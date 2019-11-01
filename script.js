b337d27e7ab01d77041ac40f0d5c2b6386fa8bdf
Andrew J Kim <andrewk745@gmail.com>
2019-10-31 21:54:59 -0400

    Cleaned up comments added event listeners

--- script.js
+++ script.js
@@ -11,16 +11,4 @@ for (i = 0; i < navElements.length; i++) {
         event.target.style.color = "#735751" 
     });
 }
-// for (i = 0; i < navElements.length; i++) {
-//     console.log(i)
-//     navElements[i].addEventListener('mouseout' , function(event) {
-//         event.target.style.color = "#e7d7c1"
-
-//     })
-// }
-
-// function changeColor() {
-//     navElements.style.color = "#8c1c13"
-    
-// }
 
